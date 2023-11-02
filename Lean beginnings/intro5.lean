fe81ccf94824fc7a05aa0400ddc720f0fd8afa0f
rafaelgrenier <rafaelmgren@arizona.edu>
2023-11-02 14:44:57 -0700

    Making more accessible

--- Lean beginnings/intro5.lean
+++ Lean beginnings/intro5.lean
@@ -1,16 +1,32 @@
 /-
 # Sets in Lean
 
-For any type α, Set α is the type which contains all sets of elements which are 
+For any type α, Set α is the type which contains all sets of elements which are
 of type α. This is actually part of how type theory was developed, as this
 construction of sets prevents Russell's paradox, since the set of all sets
 is not well-defined in the type-theoretic construction.
 
+There are two sets that always exist for any type: univ and ∅.
+univ : Set α has the property that ∀ x : α, x ∈ univ.
+∅ : Set α has the property that ∀ x : α, ¬x ∈ ∅.
+The symbol ∈ denotes inclusion in a set, and for any S : Set α and
+any x : α, either x ∈ S or x ∉ S.
 
+Let X be a type, P : X → Prop.
+Then a new set on X can be created by S := {a : X | p a} : Set X.
+S is then the set which only included a : X which make p a True.
+
+For two sets S and T, we say S ⊆ T if ∀ x : α, x ∈ S → x ∈ T.
+In fact, these are definitionally equal statements.
 -/
 import Mathlib.Init.Set
 open Set
 variable (α β : Type) (S T U : Set α) (V W : Set β)
+
+example : ∅ ⊆ (univ : Set α) := by
+  intro x
+  intro _
+  trivial
 
 example : S ∩ (T ∪ U) = (S ∩ T) ∪ (S ∩ U) := by
   apply ext
@@ -34,7 +50,3 @@ example : S ∩ (T ∪ U) = (S ∩ T) ∪ (S ∩ U) := by
     · intro ⟨xS, xU⟩
       apply And.intro xS
       apply Or.inr xU
-
-
-
-
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 This project is an introduction to lean as a supplement to a course in proof writing. 
 The project then delves into some basic topology, as an application of proof writing.
+
+To install Lean 4, go to https://leanprover-community.github.io/get_started.html
+
+Once you have lean 4 downloaded, clone this repository and use lake to install
+mathlib as a package for this project.
