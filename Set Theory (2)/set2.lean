aebc5e1ee2c6dfcc53ac787abebcc30727ba682e
rafaelgrenier <rafaelmgren@arizona.edu>
2023-11-13 13:11:35 -0700

    Prod and restructuring

---
+++ Set Theory (2)/set2.lean
@@ -0,0 +1,74 @@
+/-
+# Sets and Functions in LEAN (Pt. 2: Practice)
+
+The following sample problems are pulled from Math in Lean chapter 4, plus a few
+of my own making.
+
+-/
+import Mathlib.Data.Set.Basic
+import Mathlib.Data.Set.Lattice
+import Mathlib.Data.Set.Function
+import Mathlib.Data.Set.Image
+namespace SetPractice --This keyword just marks a local scope, so the following variables
+                      --are only defined for the following problems
+variable {α I : Type*}
+variable (s t u : Set α)
+variable (A B : I → Set α)
+open Set
+
+--MIL 4.1 Problem #1
+example : s ∩ (t ∪ u) = s ∩ t ∪ s ∩ u := by
+  sorry
+
+--MIL 4.1 Problem #2
+example : s \ (t ∪ u) ⊆ (s \ t) \ u := by
+  sorry
+
+--MIL 4.1 Problem #7
+example : s \ t ∪ t \ s = (s ∪ t) \ (s ∩ t) := by
+  sorry
+
+--MIL 4.1 Problem #11
+example : (s ∪ (⋂ i, A i)) = ⋂ i, A i ∪ s := by
+  sorry
+
+example: (s ∩ ∅ = ∅) ∧ (s ∪ ∅ = s) := by
+  sorry
+
+
+end SetPractice
+
+namespace FuncPractice
+variable {α β : Type*}
+variable (f : α → β)
+variable (s t : Set α)
+variable (u v : Set β)
+open Set
+open Function
+
+--MIL 4.2 Problem #1
+example : f '' s ⊆ v ↔ s ⊆ f ⁻¹' v := by
+  sorry
+
+example : s ⊆ f ⁻¹' (f '' s) := by
+  sorry
+
+--MIL 4.2 Problem #22
+theorem Cantor : ∀ f : α → Set α, ¬Surjective f := by
+/-
+This is a proof that no function mapping a set to ites power set can be surjective.
+The idea behind this proof is to construct a subset of the domain (here denoted S) such that
+no element from the domain can be mapped to that subset. Try proving the last two blocks!
+-/
+  intro f surjf
+  let S := { i | i ∉ f i }
+  rcases surjf S with ⟨j, h⟩
+  have h1 : j ∉ f j := by
+    intro h'
+    have : j ∉ f j := by rwa [h] at h'
+    contradiction
+  have h2 : j ∈ S := by {sorry}
+  have h3 : j ∉ S := by {sorry}
+  contradiction
+
+end FuncPractice
---
+++ Set Theory (2)/set3.lean
@@ -0,0 +1,38 @@
+/-
+# Cartesian Products
+
+Cartesian Products allow for a new way to create new sets. For sets (S : set α) and (T : set β),
+the new set S ⨯ˢ T is of type set (α × β) such that for each p ∈ S and q ∈ T,
+the pair (p, q) is in S ×ˢ T.
+-/
+import Mathlib.Data.Set.Basic
+import Mathlib.Data.Set.Prod
+variable (α β : Type)
+variable (S T : Set α) (U V : Set β)
+open Set
+
+-- here are a few examples of proofs involving Cartesian Products
+example {p : α} {q : β} (hp : p ∈ S) (hq : q ∈ U) : (p, q) ∈ S ×ˢ U := by
+  rw [mem_prod_eq]
+  dsimp
+  constructor
+  · exact hp
+  · exact hq
+
+example (hST : S ⊆ T) (hUV : U ⊆ V) : S ×ˢ U ⊆ T ×ˢ V := by
+  intro p hp
+  rw [mem_prod_eq] at *
+  constructor
+  · apply hST
+    exact And.left hp
+  · apply hUV
+    exact And.right hp
+
+-- You try!
+example {p : α} {q : β} : (p, q) ∈ S ×ˢ U ↔ (q, p) ∈ U ×ˢ S := by
+  sorry
+
+-- Cartesian Products get more interesting when we consider the product of not just two set,
+-- but a whole family of sets!
+-- "I" is
+variable (I : Type) (A B : I → Set α)
